30f809b9710c4b751b855317198237a1162f0f23
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-09-09 16:17:10 -0400

    build scripts

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../packages/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 }
