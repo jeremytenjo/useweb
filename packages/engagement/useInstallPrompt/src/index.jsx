b380457691bb604d4a557abd4aaab0e5ec3ddb2e
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-11-11 15:49:33 -0500

    created use-install-prompt

---
+++ packages/engagement/useInstallPrompt/src/index.jsx
@@ -0,0 +1,56 @@
+import React, { createContext, useContext, useRef, useEffect, useState } from 'react'
+
+export const UseInstallPromptContext = createContext(null)
+
+export const UseInstallPromptProvider = ({ children }) => {
+  const [isNotInstalledRef, setIsNotInstalled] = useState(null)
+  const deferredPromptRef = useRef(null)
+  const isNotInstalled = isNotInstalledRef === null ? false : isNotInstalledRef
+
+  const prompt = () => {
+    if (deferredPromptRef.current) {
+      deferredPromptRef.current.prompt()
+
+      deferredPromptRef.current.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          setIsNotInstalled(false)
+        }
+      })
+    }
+  }
+
+  const setPromptFunction = (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault()
+    // Stash the event so it can be triggered later.
+    deferredPromptRef.current = e
+
+    if (window.matchMedia) {
+      const appIsInstalled = window.matchMedia('(display-mode: standalone)').matches
+      setIsNotInstalled(!appIsInstalled)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', setPromptFunction)
+
+    return () => {
+      window.removeEventListener('beforeinstallprompt', setPromptFunction)
+    }
+  }, [])
+
+  return (
+    <UseInstallPromptContext.Provider
+      value={{
+        prompt,
+        isNotInstalled,
+      }}
+    >
+      {children}
+    </UseInstallPromptContext.Provider>
+  )
+}
+
+const useInstallPrompt = () => useContext(UseInstallPromptContext)
+
+export default useInstallPrompt
