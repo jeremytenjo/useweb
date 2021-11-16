668a388a87eecdc15fa3d877de3a7a28b07c528e
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-11-16 14:53:23 -0500

    taget

--- packages/navigation/Link/src/index.tsx
+++ packages/navigation/Link/src/index.tsx
@@ -6,10 +6,20 @@ import * as localStyles from './styles'
 type Props = {
   children: any
   href: string
+  title?: string
+  target?: string
+  rel?: string
   styles?: object
 }
 
-const Link = ({ children, styles = {}, ...rest }: Props) => {
+const Link = ({
+  children,
+  styles = {},
+  title,
+  target = '_blank',
+  rel = 'noopener noreferrer',
+  ...rest
+}: Props) => {
   return (
     <Box styles={{ ...localStyles.wrapper, ...styles }} {...rest} as='a'>
       {children}
