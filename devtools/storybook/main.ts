7cf685eaa2c3e9ed898d864ccd1384a26748efd2
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-11-22 13:30:18 -0500

    hi

--- devtools/storybook/main.ts
+++
@@ -1,9 +0,0 @@
-// import webpack from './webpack'
-
-module.exports = {
-  stories: ['../../packages/**/_stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-docs', '@storybook/addon-controls'],
-  // webpackFinal: async (defaultConfig) => {
-  //   return webpack(defaultConfig)
-  // },
-}
---
+++ packages/feedback/Modal/stories/sb.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Modal = () => {
+  return <div>hello</div>
+}
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'feedback/modal',
+  component: Modal,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+}
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Modal {...args} />
+
+export const Primary = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: 'Modal',
+}
