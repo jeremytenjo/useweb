4bbc3792a571d18cb68a64ff7947314c102ca88f
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-01 10:14:19 -0500

    udpated useasync to ts

--- packages/data/useAsync/src/index.ts
+++ packages/data/useAsync/src/index.ts
@@ -1,45 +1,58 @@
 import { useState, useCallback, useEffect } from 'react'
+
+type Options = {
+  autoExec?: boolean
+  onResult?: (result: any) => void
+  onError?: (error: any) => void
+  onLoading?: (loading: boolean) => void
+}
+
+type Return = {
+  loading: boolean
+  error: any
+  result: any
+  exec: (payload?: any) => void
+}
+
 /**
  * @example
  * const promise = useAsync(fetcher)
- *
- * usage example: "useJoiValidator/index.js"
  */
 export default function useAsync(
-  fetcher = () => null,
-  { autoExec, onResult, onError, onLoading } = {},
-) {
+  fetcher: (result: any) => void,
+  options?: Options,
+): Return {
   const [loading, setLoading] = useState(null)
   const [result, setResult] = useState(null)
   const [error, setError] = useState(null)
 
   const exec = useCallback(
-    async (payload) => {
+    async (payload?: any) => {
       if (loading) return null
 
       try {
         setLoading(true)
-        onLoading && onLoading(true)
+        options?.onLoading && options?.onLoading(true)
         setResult(null)
         setError(null)
         const res = await fetcher(payload)
         setResult(res)
-        onResult && onResult(res)
+        options?.onResult && options?.onResult(res)
         return res
       } catch (error) {
         setError(error)
-        onError && onError(error)
+        options?.onError && options?.onError(error)
       } finally {
         setLoading(false)
-        onLoading && onLoading(false)
+        options?.onLoading && options?.onLoading(false)
       }
     },
     [fetcher],
   )
 
   useEffect(() => {
-    if (autoExec) exec()
-  }, [exec, autoExec])
+    if (options?.autoExec) exec()
+  }, [exec, options?.autoExec])
 
   return { loading, error, result, exec }
 }
