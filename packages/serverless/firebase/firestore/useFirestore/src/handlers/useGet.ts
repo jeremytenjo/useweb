9230695a2cea8168c54d985e011988e885d3ec61
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-03 17:14:44 -0500

    start use firestore

---
+++ packages/serverless/firebase/firestore/useFirestore/src/handlers/useGet.ts
@@ -0,0 +1,138 @@
+import { useMemo } from 'react'
+import { collection, query, where, getDocs } from 'firebase/firestore'
+import useSWRImmutable from 'swr/immutable'
+import create from 'zustand'
+import arrayDB from '@useweb/array-db'
+import useFirebase from '@useweb/use-firebase'
+
+import useLocalStorage from '../../../storage/useLocalStorage'
+import type { HandlerPayloadType } from '..'
+
+type Types = {
+  fetchedCollections: any[]
+  setFetchedCollections: (newValue: any) => void
+}
+
+const useGetStore = create<Types>((set) => ({
+  fetchedCollections: [],
+  setFetchedCollections: (newValue) => set(() => ({ fetchedCollections: newValue })),
+}))
+
+type Callbacks = {
+  onGet?: (result: any) => void
+  onGetError?: (error: any) => void
+  onGetLoading?: (loading: boolean) => void
+}
+
+export default function useGet(
+  { userId, collectionName, defaultData, returnDefaultData }: HandlerPayloadType,
+  callbacks?: Callbacks,
+) {
+  const firebase = useFirebase()
+  const getStore: any = useGetStore()
+
+  const collectionWasFetched = useMemo(() => {
+    const wasCollectionFetched = getStore.fetchedCollections.some(
+      (fetchedCollection) => fetchedCollection.id === collectionName,
+    )
+    return wasCollectionFetched
+  }, [collectionName, getStore.fetchedCollections])
+
+  const firestoreFetcher = async () => {
+    const data = []
+    const q = query(
+      collection(firebase.db, collectionName),
+      where('userId', '==', userId),
+    )
+    const querySnapshot = await getDocs(q)
+
+    querySnapshot.forEach((doc) => {
+      data.push({
+        id: doc.id,
+        ...doc.data(),
+      })
+    })
+
+    return data
+  }
+
+  const updateFetchedCollections = () => {
+    const updatedFetchedCollections = arrayDB.add(getStore.fetchedCollections, {
+      data: { id: collectionName },
+    })
+
+    getStore.setFetchedCollections(updatedFetchedCollections)
+  }
+
+  const localStorageData = useLocalStorage(collectionName, {
+    onGet: (result) => {
+      updateFetchedCollections()
+      callbacks.onGet(result)
+    },
+  })
+
+  const swrKey = () => (userId ? collectionName : null)
+
+  // https://swr.vercel.app/docs/options
+  const swr = useSWRImmutable(swrKey, firestoreFetcher, {
+    onSuccess: (data) => {
+      const updatedFetchedCollections = arrayDB.add(getStore.fetchedCollections, {
+        data: { id: collectionName },
+      })
+
+      getStore.setFetchedCollections(updatedFetchedCollections)
+      localStorageData.update(data)
+      callbacks.onGet(data)
+    },
+    onError: (error) => {
+      callbacks.onGetError(error)
+    },
+  })
+
+  const update = (newData) => {
+    localStorageData.update(newData)
+
+    if (swrKey()) {
+      swr.mutate(newData, false)
+    }
+  }
+
+  const getReturnData = () => {
+    if (!swr.data && !collectionWasFetched && localStorageData.data) {
+      return localStorageData.data
+    }
+
+    if (swr.data) {
+      return swr.data
+    }
+
+    if (!swr.data && localStorageData.data) {
+      return localStorageData.data
+    }
+
+    if (
+      collectionWasFetched &&
+      !swr.data &&
+      !localStorageData.data &&
+      returnDefaultData &&
+      defaultData
+    ) {
+      return defaultData
+    }
+
+    return []
+  }
+
+  const fetching = !swr.data && !swr.error
+  const data = getReturnData()
+  const error = swr.error
+  const isFetched = collectionWasFetched
+
+  return {
+    data,
+    fetching,
+    isFetched,
+    error,
+    update,
+  }
+}
---
+++ packages/serverless/firebase/firestore/useFirestore/src/index.ts
@@ -0,0 +1,106 @@
+import useFirebase from '@useweb/use-firebase'
+
+import useGet from './handlers/useGet'
+import useCreate from './handlers/useCreate'
+import useRemove from './handlers/useRemove'
+import useUpdate from './handlers/useUpdate'
+
+export type HandlerPayloadType = {
+  userId: string
+  collectionName: string
+  returnDefaultData: any
+  defaultData: any
+  updateData: (newData: any) => void
+  data: any
+}
+
+type Options = {
+  defaultData?: any
+  returnDefaultData?: boolean
+
+  onGet?: (result: any) => void
+  onGetError?: (error: any) => void
+  onGetLoading?: (loading: boolean) => void
+
+  onCreate?: (result: any) => void
+  onCreateError?: (error: any) => void
+  onCreateLoading?: (loading: boolean) => void
+
+  onRemove?: (result: any) => void
+  onRemoveError?: (error: any) => void
+  onRemoveLoading?: (loading: boolean) => void
+
+  onUpdate?: (result: any) => void
+  onUpdateError?: (error: any) => void
+  onUpdateLoading?: (loading: boolean) => void
+}
+
+// TODO extract to npm package
+export default function useFirestore(
+  collectionName: string,
+  {
+    defaultData,
+    returnDefaultData,
+
+    onGet = () => null,
+    onGetError = () => null,
+    onGetLoading = () => null,
+
+    onCreate = () => null,
+    onCreateError = () => null,
+    onCreateLoading = () => null,
+
+    onRemove = () => null,
+    onRemoveError = () => null,
+    onRemoveLoading = () => null,
+
+    onUpdate = () => null,
+    onUpdateError = () => null,
+    onUpdateLoading = () => null,
+  }: Options,
+) {
+  const firebase = useFirebase()
+
+  const handlerPayload: HandlerPayloadType = {
+    userId: firebase.user?.uid,
+    collectionName,
+    returnDefaultData,
+    defaultData,
+    updateData: () => null,
+    data: [],
+  }
+
+  const get = useGet(handlerPayload, {
+    onGet,
+    onGetError,
+    onGetLoading,
+  })
+
+  handlerPayload.updateData = get.update
+  handlerPayload.data = get.data
+
+  const create = useCreate(handlerPayload, {
+    onCreate,
+    onCreateError,
+    onCreateLoading,
+  })
+
+  const update = useUpdate(handlerPayload, {
+    onUpdate,
+    onUpdateError,
+    onUpdateLoading,
+  })
+
+  const remove = useRemove(handlerPayload, {
+    onRemove,
+    onRemoveError,
+    onRemoveLoading,
+  })
+
+  return {
+    get,
+    update,
+    remove,
+    create,
+  }
+}
