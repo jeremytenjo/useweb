268acc3d57053e0c473a7167fda7c440641d57d0
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-02 23:20:16 -0500

    remove build before building storybok

--- devtools/storybook/scripts/buildStorybook/buildStorybook.ts
+++ devtools/storybook/scripts/buildStorybook/buildStorybook.ts
@@ -1,5 +1,12 @@
+import path from 'path'
+
 import shell from '../../../../packages/node/shell/index.js'
+import removeFolder from '../../../../packages/devtools/compiler/src/utils/removeFolder.js'
 
 export default async function buildStorybook() {
-  await shell('build-storybook -o ./devtools/storybook/build -c ./devtools/storybook')
+  const buildDirRoot = 'devtools/storybook/build'
+  const buildDir = path.join(process.cwd(), buildDirRoot)
+
+  await removeFolder(buildDir)
+  await shell(`build-storybook -o ./${buildDirRoot} -c ./devtools/storybook`)
 }
