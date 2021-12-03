b3c0c4c168a281f77771a1bc41038494b0fbd956
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-02 22:26:27 -0500

    fixed name

--- scripts/deploy/deploy.ts
+++ scripts/deploy/deploy.ts
@@ -1,5 +1,5 @@
 import buildStorybook from '../../devtools/storybook/scripts/buildStorybook/buildStorybook.js'
-import deployHosting from '../../firebase/scrips/deployHosting/deployHosting.js'
+import deployHosting from '../../firebase/scripts/deployHosting/deployHosting.js'
 ;(async function devScript() {
   await buildStorybook()
   deployHosting()
