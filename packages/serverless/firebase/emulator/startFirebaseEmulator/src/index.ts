58f195e66c3ddece004cd1f9fbf07936db0d2ee2
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-03 23:07:49 -0500

    enabled fix initializeFirebaseEmulator

--- packages/serverless/firebase/emulator/startFirebaseEmulator/src/index.ts
+++ packages/serverless/firebase/emulator/startFirebaseEmulator/src/index.ts
@@ -3,6 +3,7 @@ import startAuthEmulator from './handlers/startAuthEmulator'
 import startFirestoreEmulator from './handlers/startFirestoreEmulator'
 
 type Props = {
+  enable: boolean
   auth?: any
   db?: any
   authEmulatorPort?: number
@@ -10,13 +11,14 @@ type Props = {
 }
 
 export default function startFirebaseEmulator({
+  enable,
   auth,
   db,
   authEmulatorPort = 9005,
   dbEmulatorPort = 9003,
 }: Props) {
-  const enabled = !!auth || !!db
-  if (process.env.NODE_ENV !== 'development' && enabled) return
+  const enabled = enable && (!!auth || !!db)
+  if (!enabled) return
 
   auth && startAuthEmulator({ auth, authEmulatorPort })
   db && startFirestoreEmulator({ db, dbEmulatorPort })
