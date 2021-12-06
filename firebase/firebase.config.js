cfa97d7e16995aaf50dce6aa5808f3052d34a572
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-06 09:11:27 -0500

    firebase config

---
+++ firebase/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyAMsN6u5O-ZCe98A4Dmgngpjqgoh5x3IVU',
+  authDomain: 'useweb-lib.firebaseapp.com',
+  projectId: 'useweb-lib',
+  storageBucket: 'useweb-lib.appspot.com',
+  messagingSenderId: '1029586460785',
+  appId: '1:1029586460785:web:1a8a3a11bf3c70208ffbd0',
+  measurementId: 'G-SZFJ3ET6B0',
+}
