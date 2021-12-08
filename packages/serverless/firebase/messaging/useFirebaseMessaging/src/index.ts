44965a70d622407c14a3378bc5c50a2dae34ea24
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-07 23:17:11 -0500

    fix paylod

--- packages/serverless/firebase/messaging/useFirebaseMessaging/src/index.ts
+++ packages/serverless/firebase/messaging/useFirebaseMessaging/src/index.ts
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
 import useFirebase from '@useweb/use-firebase'
-import { getToken, onMessage } from 'firebase/messaging'
+import { getToken, onMessage as messagingOnMessage } from 'firebase/messaging'
 
 const isProduction = () => process.env.NODE_ENV === 'production'
 
@@ -103,7 +103,10 @@ export default function useFirebaseMessaging({
     try {
       const token = await getToken(firebase.messaging, { vapidKey })
       token && setFcmRegistrationToken(token)
-      onMessageRemoveListenerRef.current = onMessage(onMessage)
+      onMessageRemoveListenerRef.current = messagingOnMessage(
+        firebase.messaging,
+        onMessage,
+      )
     } catch (error) {
       setError(error)
       onError(error)
