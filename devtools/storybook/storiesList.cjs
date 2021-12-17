f416d4394c083e77b2a598a6797a2d06368ba323
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-12-17 12:35:52 -0500

    updated

--- devtools/storybook/storiesList.cjs
+++ devtools/storybook/storiesList.cjs
@@ -8,10 +8,9 @@ module.exports = [
   '../../packages/data/useData/stories/stories.tsx',
   '../../packages/dataDisplay/Carousel/stories/stories.tsx',
   '../../packages/feedback/Modal/stories/stories.tsx',
-  '../../packages/serverless/firebase/functions/firebase/useFirebaseFunction/node_modules/@useweb/use-firebase/stories/stories.tsx',
+  '../../packages/serverless/firebase/auth/useFirebaseAuth/node_modules/@useweb/use-firebase/stories/stories.tsx',
+  '../../packages/serverless/firebase/auth/useFirebaseAuth/stories/stories.tsx',
   '../../packages/serverless/firebase/functions/firebase/useFirebaseFunction/stories/stories.tsx',
-  '../../packages/serverless/firebase/messaging/useFirebaseMessaging/node_modules/@useweb/use-firebase/stories/stories.tsx',
   '../../packages/serverless/firebase/messaging/useFirebaseMessaging/stories/stories.tsx',
-  '../../packages/serverless/firebase/useFirebase/node_modules/@useweb/use-firebase-messaging/stories/stories.tsx',
   '../../packages/serverless/firebase/useFirebase/stories/stories.tsx',
 ]
---
+++ packages/serverless/firebase/auth/useFirebaseAuth/stories/stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Docs from './docs.mdx'
+
+export default {
+  title: 'packages/Firebase/Auth/useFirebaseAuth',
+  args: {
+    show: null,
+  },
+  // https://storybook.js.org/docs/react/writing-docs/docs-page#remixing-docspage-using-doc-blocks
+  parameters: {
+    docs: Docs,
+  },
+}
