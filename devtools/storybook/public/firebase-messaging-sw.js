d2e0a4c67b31a0466cd13733df5be26cc7322d3d
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-01-05 17:45:09 -0500

    c

--- devtools/storybook/public/firebase-messaging-sw.js
+++ devtools/storybook/public/firebase-messaging-sw.js
@@ -16,10 +16,10 @@ const messaging = firebase.messaging()
 
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload)
-  const notificationTitle = payload.title
+  const notificationTitle = payload.notification.title
   const notificationOptions = {
-    body: payload.body,
-    icon: payload.icon || payload.image,
+    body: payload.notification.body,
+    icon: payload.notification.icon || payload.notification.image,
   }
 
   self.registration.showNotification(notificationTitle, notificationOptions)
