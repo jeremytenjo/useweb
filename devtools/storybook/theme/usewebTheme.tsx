3bc66ec6d66bbfb72424a68d30dae6540d8d9fb5
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-11 18:25:12 -0400

    c

--- devtools/storybook/theme/usewebTheme.tsx
+++ devtools/storybook/theme/usewebTheme.tsx
@@ -6,19 +6,17 @@ import colors from './tokens/colors'
 import { variants as typography } from './tokens/typography/typography'
 
 const ButtonDefaults = {
-  MuiButton: {
-    styleOverrides: {
-      root: {
-        borderRadius: '10px',
-        fontWeight: 'bold',
-        width: '100%',
-        textTransform: 'none' as const,
-        fontFamily: 'PoppinsRegular',
-        color: 'black.main',
+  styleOverrides: {
+    root: {
+      borderRadius: '10px',
+      fontWeight: 'bold',
+      width: '100%',
+      textTransform: 'none' as const,
+      fontFamily: 'PoppinsRegular',
+      color: 'black.main',
+      backgroundColor: 'white',
+      '&:hover': {
         backgroundColor: 'white',
-        '&:hover': {
-          backgroundColor: 'white',
-        },
       },
     },
   },
@@ -28,7 +26,7 @@ const theme = createTheme({
   palette: colors,
   typography,
   components: {
-    ...ButtonDefaults,
+    Button: ButtonDefaults,
   },
 })
 
--- packages/theme/UsewebTheme/src/createTheme.ts
+++ packages/theme/UsewebTheme/src/createTheme.ts
@@ -1,9 +1,47 @@
+import { Object as TSObject } from 'ts-toolbelt'
 import {
   createTheme as createMuiTheme,
   ThemeOptions as MuiThemeOptions,
 } from '@mui/material/styles'
 
-export default function createTheme(themeData: MuiThemeOptions) {
-  const createdTheme = createMuiTheme(themeData)
+type ComponentProps = {
+  defaultProps?: any
+  styleOverrides?: any
+  variants?: any[]
+}
+
+export type CreateThemeProps = TSObject.P.Update<
+  MuiThemeOptions,
+  ['components'],
+  ComponentsProps
+>
+/**
+ * [API](https://mui.com/material-ui/customization/theme-components/#global-style-overrides)
+ */
+export default function createTheme(themeData: CreateThemeProps) {
+  const components = getComponents({ themeComponents: themeData.components })
+  themeData.components = components as any
+  const createdTheme = createMuiTheme(themeData as any)
   return createdTheme
 }
+
+const getComponents = ({ themeComponents = {} as ComponentsProps }) => {
+  // List of component that use @mui/material/* internally
+  const componentsWithMuiPrefix = {}
+  for (const [key, value] of Object.entries(themeComponents)) {
+    const isMuiComponent = muiComponents.includes(key)
+    const name = isMuiComponent ? `Mui${key}` : key
+    componentsWithMuiPrefix[name] = value
+  }
+  return componentsWithMuiPrefix
+}
+
+// Component list
+type ComponentsProps = {
+  CssBaseline?: ComponentProps
+  Button?: ComponentProps
+  Dialog?: ComponentProps
+}
+
+// List of component that use @mui/material/* internally
+const muiComponents = ['CssBaseline', 'Button', 'Dialog']
