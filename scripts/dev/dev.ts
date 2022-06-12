4e12edf8d3537a50cbdaf7363b20e31f3c936c7c
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-12 17:40:57 -0400

    delinked

--- scripts/dev/dev.ts
+++ scripts/dev/dev.ts
@@ -1,7 +1,5 @@
 import startStorybook from '../../devtools/storybook/scripts/startStorybook/startStorybook.js'
-import shell from '../../packages/node/shell/index.js'
 
 export default async function devApp() {
-  await shell('npm run lerna:link')
   startStorybook()
 }
