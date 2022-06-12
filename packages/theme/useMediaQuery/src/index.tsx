2eec3c874f974c0a8868fc3476430120a5ba9816
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-12 11:16:06 -0400

    @useweb/use-media-query

--- packages/theme/useMediaQuery/src/index.tsx
+++ packages/theme/useMediaQuery/src/index.tsx
@@ -1,4 +1,4 @@
-import { useTheme } from '@mui/material/styles'
+import useTheme from '@mui/material/styles/useTheme'
 import useMediaQueryMui from '@mui/material/useMediaQuery'
 
 export type UseMediaQueryProps = {
