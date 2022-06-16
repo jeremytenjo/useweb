07ac934837dc29baef7f209b5271331a895c6927
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-16 10:54:58 -0400

    optimized deploy script

--- scripts/deploy/deploy.ts
+++ scripts/deploy/deploy.ts
@@ -1,17 +1,11 @@
 import chalk from 'chalk'
 
 import buildApp from '../buildApp/buildApp.js'
-import shell from '../../packages/node/shell/shell.js'
 import deployFirebase from '../../firebase/scripts/deployFirebase/deployFirebase.js'
 
 export default async function deployApp() {
   await buildApp()
-
   console.log('')
-  await shell('git add --all')
-  await shell('git commit -m "built storybook"')
-  console.log('')
-
   await deployFirebase()
 
   console.log('')
