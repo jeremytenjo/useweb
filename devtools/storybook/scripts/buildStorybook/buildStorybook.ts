8fe3baf11dbdf3e6af7e3486fdfed4c79c5da685
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-16 09:12:53 -0400

    track storybookBuild

--- devtools/storybook/scripts/buildStorybook/buildStorybook.ts
+++ devtools/storybook/scripts/buildStorybook/buildStorybook.ts
@@ -6,7 +6,7 @@ import shell from '../../../../packages/node/shell/shell.js'
 import removeFolder from '../../../../packages/devtools/compiler/src/utils/removeFolder.js'
 
 export default async function buildStorybook() {
-  const buildDirRoot = 'devtools/storybook/build'
+  const buildDirRoot = 'devtools/storybook/storybookBuild'
   const buildDir = path.join(process.cwd(), buildDirRoot)
 
   await removeFolder(buildDir)
