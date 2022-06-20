e268061c74c307ee8cd294808277cb170b98d533
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-20 08:24:52 -0400

    @useweb/firebase-function-tester@2.1.0

--- packages/dataDisplay/FetchingUi/stories/FetchingUi.stories.tsx
+++ packages/dataDisplay/FetchingUi/stories/FetchingUi.stories.tsx
@@ -5,7 +5,28 @@ import { FetchingUi, type FetchingUiProps } from './component'
 import Docs from './FetchingUi.docs'
 
 const defaultArgs: FetchingUiProps = {
-  title: 'Data Display/FetchingUi',
+  result: {
+    example1: [
+      {
+        name: 'test01',
+        age: 18,
+        gender: 0,
+        student: true,
+        children: null,
+      },
+      {
+        name: 'test02',
+        age: 19,
+        gender: 1,
+        student: true,
+        children: null,
+      },
+    ],
+    example2: {
+      friuts: ['apple', 'grape', 'jujube', 'pear'],
+      transport: ['taxi', 'bus', 'metro', 'plane', 'train'],
+    },
+  },
 }
 
 export default {
--- packages/serverless/firebase/functions/FirebaseFunctionTester/stories/FirebaseFunctionTester.stories.tsx
+++ packages/serverless/firebase/functions/FirebaseFunctionTester/stories/FirebaseFunctionTester.stories.tsx
@@ -1,18 +1,15 @@
 //https://storybook.js.org/docs/react/writing-docs/docs-page
-import React from "react";
+import React from 'react'
 
-import {
-  FirebaseFunctionTester,
-  type FirebaseFunctionTesterProps,
-} from "./component";
-import Docs from "./FirebaseFunctionTester.docs";
+import { FirebaseFunctionTester, type FirebaseFunctionTesterProps } from './component'
+import Docs from './FirebaseFunctionTester.docs'
 
 const defaultArgs: FirebaseFunctionTesterProps = {
-  name: "FirebaseFunctionTester",
-};
+  functionName: 'helloWorld',
+}
 
 export default {
-  title: "FirebaseFunctionTester",
+  title: 'serverless/firebase/Functions/FunctionTester',
   args: defaultArgs,
   // https://storybook.js.org/docs/react/writing-docs/docs-page#remixing-docspage-using-doc-blocks
   parameters: {
@@ -20,21 +17,21 @@ export default {
       page: Docs,
     },
   },
-};
+}
 
 const Template = (args) => {
   return (
     <>
       <FirebaseFunctionTester {...args} />
     </>
-  );
-};
+  )
+}
 
 export const Default = {
   render: (args) => {
-    return <Template {...args} />;
+    return <Template {...args} />
   },
-};
+}
 
 // const variantArgs: FirebaseFunctionTesterProps = {
 //  name: 'World',
