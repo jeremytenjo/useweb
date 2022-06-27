86d62dff639fbefabc458c4bdaf325ef33a4d951
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-06-27 15:52:51 -0400

    list: remove ads support

--- packages/_ui/src/dataDisplay/List/src/index.tsx
+++ packages/_ui/src/dataDisplay/List/src/index.tsx
@@ -1,13 +1,10 @@
 import React, { Fragment } from 'react'
 import Box, { type BoxProps } from '@mui/material/Box'
 
-// import AdUnit from '../googleAnalytics/AdUnit/AdUnit'
-
 export type ListProps = {
   data: any[]
   ListItemComponent: any
   listItemProps?: any
-  enableAds?: boolean
   sx?: BoxProps['sx']
   onItemClick?: (data: { data: any }) => any
 }
@@ -16,24 +13,22 @@ export default function List({
   data = [],
   ListItemComponent,
   listItemProps = {},
-  enableAds = false,
   sx = {},
   onItemClick = () => null,
 }: ListProps) {
   return (
-    <Wrapper sx={sx as any}>
+    <Wrapper sx={sx as any} data-id='List'>
       <Items
         data={data}
         ListItemComponent={ListItemComponent}
         listItemProps={listItemProps}
-        enableAds={enableAds}
         onItemClick={onItemClick}
       />
     </Wrapper>
   )
 }
 
-const Wrapper = ({ children, sx = {} }) => {
+const Wrapper = ({ children, sx = {} as ListProps['sx'] }) => {
   return (
     <Box
       component='ul'
@@ -44,38 +39,19 @@ const Wrapper = ({ children, sx = {} }) => {
   )
 }
 
-const Items = ({
-  data,
-  ListItemComponent,
-  listItemProps = {},
-  enableAds,
-  onItemClick,
-}) => {
+const Items = ({ data, ListItemComponent, listItemProps = {}, onItemClick }) => {
   return data.map((item, index) => {
     return (
       <Fragment key={item.id + Math.random() + index}>
         <Box
           component='li'
+          onClick={(e) => onItemClick({ event: e, data: item })}
           sx={{
             listStyle: 'none',
           }}
-          onClick={(e) => onItemClick({ e, data: item })}
         >
           <ListItemComponent index={index} {...item} {...listItemProps} />
         </Box>
-
-        {enableAds && index === 7 && (
-          <Box
-            component='li'
-            sx={{
-              // TODO set grid column based on amount of items, eg 3 break
-              gridColumn: '1/5',
-              height: '345px',
-            }}
-          >
-            {/* <AdUnit /> */}
-          </Box>
-        )}
       </Fragment>
     )
   })
