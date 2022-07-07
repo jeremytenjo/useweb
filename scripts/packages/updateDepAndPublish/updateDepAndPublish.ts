a55d390462a6f0d6c5547b9714dceb1d895fc0d2
Jeremy Tenjo <tenjojeremy@gmail.com>
2022-07-07 12:36:40 -0400

    ex

--- scripts/packages/updateDepAndPublish/updateDepAndPublish.ts
+++ scripts/packages/updateDepAndPublish/updateDepAndPublish.ts
@@ -3,6 +3,7 @@ import task from '../../../devtools/utils/task/task.js'
 import enquireDep from './handlers/enquireDep/enquireDep.js'
 import updatePackagesWithDep from './handlers/updatePackagesWithDep/updatePackagesWithDep.js'
 
+// updates dependency and deploys a package with a selected dependency
 export default async function updateDepAndPublish() {
   const depToUpdate = await enquireDep()
 
